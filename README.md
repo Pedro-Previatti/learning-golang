83ee1428fcabbba5dc26297694a64c213a3628fc
Pedro Vitor <pvlpreviatti@gmail.com.br>
2024-01-22 13:14:54 -0300

    main.go - learning variables

---
+++ README.md
@@ -0,0 +1,4 @@
+# Learning golang sintax and features
+
+## Variables 
+Learned to name variables and creating main structure.
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// creating a variable of type int
+	var num int
+	// assingning value of 10 to variable num
+	num = 10
+	// creating a variable and assigning an int value
+	i := 10
+
+	// printing variables
+	fmt.Println(num)
+	fmt.Println(i)
+}
