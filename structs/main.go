f38ead1afa11b51e60575114bfef4651ce62c8d8
Pedro Previatti <pvlpreviatti@gmail.com>
2024-01-24 10:14:37 -0300

    pointers

--- structs/main.go
+++ structs/main.go
@@ -2,27 +2,39 @@ package main
 
 import "fmt"
 
+type contact struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
-	email     string
-	zipCode   int
+	contact
 }
 
 func main() {
 	p := person{
-		firstName: "Pedro",
+		firstName: "Peter",
 		lastName:  "Previatti",
-		email:     "pvlpreviatti@gmail.com",
-		zipCode:   19191919,
+		contact: contact{
+			email:   "pvlpreviatti@gmail.com",
+			zipCode: 19191919,
+		},
 	}
 
+	ptr := &p
+	ptr.updateName("Pedro")
 	p.print()
 }
 
 func (p person) print() {
 	fmt.Printf("\nFirst Name: %v", p.firstName)
 	fmt.Printf("\nLast Name: %v", p.lastName)
-	fmt.Printf("\nEmail: %v", p.email)
-	fmt.Printf("\nZip Code: %v", p.zipCode)
+	fmt.Printf("\nEmail: %v", p.contact.email)
+	fmt.Printf("\nZip Code: %v", p.contact.zipCode)
+}
+
+func (ptr *person) updateName(newFirstName string) {
+	ptr.firstName = newFirstName
 }
