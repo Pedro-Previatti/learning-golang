9f5c96851006e29eeb2b7ae39d51d35160c2e72b
Pedro Previatti <pvlpreviatti@gmail.com>
2024-01-26 10:18:14 -0300

    Write function

--- http/main.go
+++ http/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+// type logWriter will get a function
+type logWriter struct{}
 
 func main() {
 	// http.Get() returns a pointer to a Response and an Error (the return status of the http get request)
@@ -14,12 +18,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	// creating a byte slice to implement Read function
-	bs := make([]byte, 99_999)
+	lw := logWriter{}
 
-	// reading the body of Response from the http and storing it in the byte slice
-	resp.Body.Read(bs)
+	// making a one liner to get the same body response
+	io.Copy(lw, resp.Body)
+}
 
-	// now we are printing the html from the body of the response
+// writing a Write function so that logWriter struct gets the Writer interface
+func (logWriter) Write(bs []byte) (int, error) {
+	// normal implementation of Write function
 	fmt.Println(string(bs))
+
+	// custom line to show the ammount of bytes writen
+	fmt.Println("Number of bytes", len(bs))
+
+	return len(bs), nil
 }
