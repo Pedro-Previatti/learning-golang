cd032779717f3c267b02def597c61421508ac5e7
Pedro Previatti <pvlpreviatti@gmail.com>
2024-01-26 14:45:28 -0300

    Channel receiving only one Response

--- channels/main.go
+++ channels/main.go
@@ -15,19 +15,28 @@ func main() {
 		"https://amazon.com",
 	}
 
+	// channels can only communicate one type
+	c := make(chan string) // creating a channel of strings
+
 	for _, link := range links {
-		checkTrafic(link)
+		go checkTrafic(link, c)
 	}
+
+	fmt.Println(<-c)
 }
 
+// checking requests are slow, because it waits a response for the previous request
 // checking trafic from link
-func checkTrafic(s string) {
-	_, err := http.Get(s)
+func checkTrafic(s string, c chan string) {
+	_, err := http.Get(s) // blocking call
 	// if the get request's status is not 200 err is returned not null so site is probably down
 	if err != nil {
 		fmt.Println("Status check for:", s, "DOWN")
+		c <- "DOWN"
 		return
 	}
 
 	fmt.Println("Status check for:", s, "OK")
+	c <- "OK"
+
 }
